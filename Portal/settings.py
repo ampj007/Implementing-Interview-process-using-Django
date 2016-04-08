519ad303e804dfdd9ac8191e6d2275b667b618a1
ampj007 <jamp040723@gmail.com>
2016-04-08 17:25:35 +0530

    fix1

--- Portal/settings.py
+++ Portal/settings.py
@@ -10,8 +10,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+import dj_database_url
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
--- Portal/urls.py
+++ Portal/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+admin.autodiscover()
+
+import Portal.views
 
 urlpatterns = patterns('',
     # Examples:
